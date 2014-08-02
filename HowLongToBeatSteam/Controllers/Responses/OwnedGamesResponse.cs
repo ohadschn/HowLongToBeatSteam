817cebc1d153e1b3fe79e6f50f2e19048e81ba7e
ohadsc <ohad188@gmail.com>
2014-08-02 18:49:26 +0300

    Async controller

---
+++ HowLongToBeatSteam/Controllers/Responses/OwnedGamesResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace HowLongToBeatSteam.Controllers.Responses
+{
+    // ReSharper disable InconsistentNaming
+    public class OwnedGamesResponse
+    {
+        public OwnedGames response;
+    }
+    public class OwnedGames
+    {
+        public int game_count;
+        public OwnedGame[] games;
+    }
+    public class OwnedGame
+    {
+        public int appid;
+        public string name;
+        public int? playtime_2weeks;
+        public int playtime_forever;
+        public string img_icon_url;
+        public string img_logo_url;
+        public bool? has_community_visible_stats;
+    }
+    // ReSharper restore InconsistentNaming
+}
---
+++ HowLongToBeatSteam/Models/Game.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace HowLongToBeatSteam.Models
+{
+    [DataContract]
+    public class Game
+    {
+        [DataMember]
+        public int SteamAppId { get; private set; }
+        [DataMember]
+        public string SteamName { get; private set; }
+        [DataMember]
+        public int Playtime { get; private set; }
+
+        public Game(int steamAppId, string steamName, int playtime)
+        {
+            SteamAppId = steamAppId;
+            Playtime = playtime;
+            SteamName = steamName;
+        }
+    }
+}
