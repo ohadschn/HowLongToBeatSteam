a1bc44594058f54c60a23a5fad32718cbe628b92
ohadsc <ohad188@gmail.com>
2014-08-11 00:47:52 +0300

    Steam app enumeration working

---
+++ HowLongToBeatSteam/Scripts/app.js
@@ -0,0 +1,22 @@
+﻿function AppViewModel() {
+    var self = this;
+
+    self.steamId = ko.observable();
+
+    self.games = ko.observableArray();
+
+    this.howlongClicked = function () {
+
+        $.get("api/games/" + self.steamId())
+            .done(function(data) {
+                self.games(data);
+            })
+            .fail(function(error) {
+                self.games({ SteamAppId: error, SteamName: error, Playtime: error });
+        });
+    };
+}
+
+$(document).ready(function () {
+    ko.applyBindings(new AppViewModel());
+});
