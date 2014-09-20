4e1622cef1e86f95495a02bd357fadd6303714f5
ohad188@gmail.com <ohad188@gmail.com>
2014-09-20 21:26:12 +0300

    Added dummy data for testing. Added game exclusion. Moved all total logic to a single method for performance. Made alert dismissal inline in html. Fixed !important css hack. Shortened input. Minified CSS.

--- HowLongToBeatSteam/Scripts/app.js
+++ HowLongToBeatSteam/Scripts/app.js
@@ -11,6 +11,8 @@ function Game(steamGame) {
         self.known = steamGame.HltbInfo.Id !== -1;
     }
 
+    self.included = ko.observable(self.inCache);
+
     self.steamAppId = steamGame.SteamAppId;
     self.steamName = steamGame.SteamName;
     self.steamPlaytime = steamGame.Playtime;
@@ -39,37 +41,77 @@ function AppViewModel() {
         self.badSteamId64(false);
     });
 
-    self.games = ko.observableArray();
+    self.games = ko.observableArray([
+    new Game({
+        "SteamAppId": 3830,
+        "SteamName": "Psychonauts",
+        "Playtime": 0,
+        "HltbInfo": {
+            "Id": 7372,
+            "Name": "Psychonauts",
+            "MainTtb": 750,
+            "ExtrasTtb": 960,
+            "CompletionistTtb": 1320,
+            "CombinedTtb": 930
+        }
+    }),
+    new Game({
+        "SteamAppId": 228200,
+        "SteamName": "Company of Heroes (New Steam Version)",
+        "Playtime": 64,
+        "HltbInfo": {
+            "Id": -1,
+            "Name": null,
+            "MainTtb": -1,
+            "ExtrasTtb": -1,
+            "CompletionistTtb": -1,
+            "CombinedTtb": -1
+        }
+    }),
+    new Game({
+        "SteamAppId": 6200,
+        "SteamName": "Ghost Master",
+        "Playtime": 0,
+        "HltbInfo": null,
+        }
+    )]);
+
     self.processing = ko.observable(false);
+    self.toggling = ko.observable("");
     self.error = ko.observable(null);
 
-    self.missingIds = ko.computed(function() {
-        return ko.utils.arrayFirst(self.games(), function(game) {
-            return !game.known;
-        }) !== null;
-    });
-
-    self.notInCache = ko.computed(function() {
-        return ko.utils.arrayFirst(self.games(), function(game) {
-            return !game.inCache;
-        }) !== null;
-    });
-
-    self.gameCount = ko.computed(function() {
-        return self.games().length;
-    });
-
     self.missingIdsAlertHidden = ko.observable(false);
-    self.hideMissingIdsAlert = function() { self.missingIdsAlertHidden(true); };
-
     self.notInCacheAlertHidden = ko.observable(false);
-    self.hideNotInCacheAlert = function() { self.notInCacheAlertHidden(true); };
-
     self.errorAlertHidden = ko.observable(false);
-    self.hideErrorAlert = function() { self.errorAlertHidden(true); };
+
+    self.toggleAllChecked = ko.observable(true);
+    self.toggleAllCore = function() {
+        ko.utils.arrayForEach(self.games(), function(game) {
+            if (game.inCache) {
+                game.included(self.toggleAllChecked());
+            }
+        });
+    };
+    self.toggleAll = function () {
+        if (self.games().length < 100) {
+            self.toggleAllCore();
+            return true;
+        };
+
+        $('#togglingModal').modal();
+        setTimeout(function() {
+            self.toggleAllCore();
+            $('#togglingModal').modal("hide");
+            $('#toggleAll').prop('checked', self.toggleAllChecked()); //for some reason knockout doesn't bring it back in sync
+        }, 0);
+        return false;
+    };
 
     self.total = ko.computed(function () {
 
+        var count = 0;
+        var missingIdsCount = 0;
+        var notInCacheCount = 0;
         var totalPlaytime = 0;
         var totalMain = 0;
         var totalExtras = 0;
@@ -79,12 +121,19 @@ function AppViewModel() {
         var extrasRemaining = 0;
         var completionistRemaining = 0;
         var combinedRemaining = 0;
-
         var length = self.games().length;
         var arr = self.games();
 
         for (var i = 0; i < length; ++i) {
             var game = arr[i];
+            missingIdsCount += !game.known;
+            notInCacheCount += !game.inCache;
+
+            if (!game.included()) {
+                continue;
+            }
+
+            count++;
             totalPlaytime += game.steamPlaytime;
             totalMain += game.hltbInfo.mainTtb;
             totalExtras += game.hltbInfo.extrasTtb;
@@ -96,7 +145,16 @@ function AppViewModel() {
             combinedRemaining += Math.max(0, game.hltbInfo.combinedTtb - game.steamPlaytime);
         }
 
+        if (count === length - notInCacheCount) {
+            self.toggleAllChecked(true);
+        } else if (count === 0) {
+            self.toggleAllChecked(false);
+        }
+
         return {
+            count: count,
+            missingIds: missingIdsCount > 0,
+            notInCache: notInCacheCount > 0,
             totalPlaytime: totalPlaytime,
             totalMain: totalMain,
             totalExtras: totalExtras,
