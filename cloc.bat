a217ed0a61dee9f567a278586ede19f95da4b972
Ohad Schneider <ohad188@gmail.com>
2015-08-05 23:43:17 +0300

    cloc (count lines of code)

---
+++ cloc.bat
@@ -0,0 +1,1 @@
+C:\Utilities\cloc\cloc-1.64.exe . --counted=counted.txt --exclude-ext=csproj,config,bat,cmd,json,min.js,min.css --exclude-dir="Enterprise Library Semantic Logging Service",".nuget","Properties","bin","obj","Intellisense","bower_components","packages"
