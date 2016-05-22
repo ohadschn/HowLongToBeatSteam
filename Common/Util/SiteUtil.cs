97f8d1743bb1be8e67eeff7e9277429e9501ec8f
Ohad Schneider <ohad188@gmail.com>
2016-05-22 16:31:47 +0300

    Send success mail to contact@howlongtobeatsteam.com

--- Common/Util/SiteUtil.cs
+++ Common/Util/SiteUtil.cs
@@ -27,7 +27,6 @@ namespace Common.Util
     {
         private static readonly string SendGridUser = GetMandatoryValueFromConfig("SendGridUser");
         private static readonly string SendGridPassword = GetMandatoryValueFromConfig("SendGridPassword");
-        private static readonly string NotificationEmailAddress = GetMandatoryValueFromConfig("NotificationEmailAddress");
         private static readonly int SendGridRetries = GetOptionalValueFromConfig("SendGridRetries", 10);
 
         private const string WebjobNameEnvironmentVariable = "WEBJOBS_NAME";
@@ -397,7 +396,7 @@ namespace Common.Util
                     From = new MailAddress("webjobs@howlongtobeatsteam.com", "HLTBS WebJob notifier"),
                     Subject = String.Format(CultureInfo.InvariantCulture, "{0} - Success ({1}) [{2}]", 
                         WebJobName, duration, message + (errors.Length == 0 ? String.Empty : " (with session errors)")),
-                    To = new[] {new MailAddress(NotificationEmailAddress)},
+                    To = new[] {new MailAddress("contact@howlongtobeatsteam.com") },
                     Text = String.Format(CultureInfo.InvariantCulture, "{1}{0}Run ID: {2}{0}Start time: {3}{0}End time:{4}{0}Output log file: {5}{6}",
                         Environment.NewLine, GetTriggeredRunUrl(), WebJobRunId, DateTime.UtcNow - duration, DateTime.UtcNow, GetTriggeredLogUrl(),
                         errors.Length == 0 ? String.Empty : String.Format("{0}Session Errors:{0}{1}", Environment.NewLine, errorsText))
