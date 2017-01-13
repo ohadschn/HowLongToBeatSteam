5b6a43f84fb1cfc7a443a9aa2aa60f639227f979
ohadschn <ohad188@gmail.com>
2017-01-13 19:57:47 +0200

    Migrated scraper to HLTB https (HTTP endpoint no longer works)

--- SteamHltbScraper/Scraper/HltbScraper.cs
+++ SteamHltbScraper/Scraper/HltbScraper.cs
@@ -22,10 +22,10 @@ namespace SteamHltbScraper.Scraper
 {
     public static class HltbScraper
     {
-        private static readonly Uri SearchHltbUrl = new Uri(@"http://howlongtobeat.com/search_main.php?t=games&page=1&sorthead=popular&sortd=Normal%20Order&plat=&detail=0");
+        private static readonly Uri SearchHltbUrl = new Uri(@"https://howlongtobeat.com/search_main.php?t=games&page=1&sorthead=popular&sortd=Normal%20Order&plat=&detail=0");
         private const string SearchHltbPostDataFormat = @"queryString={0}";
 
-        public const string HltbGamePageFormat = @"http://www.howlongtobeat.com/game.php?id={0}";
+        public const string HltbGamePageFormat = @"https://www.howlongtobeat.com/game.php?id={0}";
 
         private static readonly int ScrapingLimit = SiteUtil.GetOptionalValueFromConfig("ScrapingLimit", int.MaxValue);
         private static readonly int ScrapingRetries = SiteUtil.GetOptionalValueFromConfig("HltbScraperScrapingRetries", 5);
