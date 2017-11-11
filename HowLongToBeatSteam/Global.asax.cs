579a630c84bbdebec94bfe89aa4f2a2deada0e6a
ohadschn <ohad188@gmail.com>
2017-11-11 15:31:38 +0200

    Application Insights - moved to code-based configuration

--- HowLongToBeatSteam/Global.asax.cs
+++ HowLongToBeatSteam/Global.asax.cs
@@ -5,6 +5,7 @@ using Common.Util;
 using HowLongToBeatSteam.Controllers;
 using System.Web.Mvc;
 using System.Web.Routing;
+using HowLongToBeatSteam.Telemetry;
 
 namespace HowLongToBeatSteam
 {
@@ -12,6 +13,7 @@ namespace HowLongToBeatSteam
     {
         protected void Application_Start()
         {
+            TelemetryManager.Setup("d0a63409-84bf-4f88-a8cf-8440ce670471");
             SiteUtil.SetDefaultConnectionLimit();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             GamesController.StartUpdatingCache(); //make sure caching starts as soon as site is up
---
+++ HowLongToBeatSteam/Telemetry/TelemetryManager.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.ApplicationInsights.DependencyCollector;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.QuickPulse;
+using Microsoft.ApplicationInsights.Web;
+using Microsoft.ApplicationInsights.WindowsServer;
+using Microsoft.ApplicationInsights.WindowsServer.TelemetryChannel;
+
+namespace HowLongToBeatSteam.Telemetry
+{
+    public static class TelemetryManager
+    {
+        private const string AdaptiveSamplingType = "Event";
+        private const int AdaptiveSamplingMaxItemsPerSecond = 5;
+
+        public static void Setup(string instrumentationKey)
+        {
+            SetInstrumentationKey(instrumentationKey);
+
+            //AddAndInitializeModule<DeveloperModeWithDebuggerAttachedTelemetryModule>(); TODO enable when made public in the SDK
+
+            AddAndInitializeModule<UnhandledExceptionTelemetryModule>();
+            AddAndInitializeModule<UnobservedExceptionTelemetryModule>();
+            AddAndInitializeModule<ExceptionTrackingTelemetryModule>();
+            AddAndInitializeModule<AspNetDiagnosticTelemetryModule>();
+
+            AddAndInitializeModule(() => new RequestTrackingTelemetryModule
+            {
+                Handlers =
+                {
+                   "System.Web.Handlers.TransferRequestHandler",
+                   "Microsoft.VisualStudio.Web.PageInspector.Runtime.Tracing.RequestDataHttpHandler",
+                   "System.Web.StaticFileHandler",
+                   "System.Web.Handlers.AssemblyResourceLoader",
+                   "System.Web.Optimization.BundleHandler",
+                   "System.Web.Script.Services.ScriptHandlerFactory",
+                   "System.Web.Handlers.TraceHandler",
+                   "System.Web.Services.Discovery.DiscoveryRequestHandler",
+                   "System.Web.HttpDebugHandler"
+                }
+            });
+
+            AddAndInitializeModule(() => new DependencyTrackingTelemetryModule
+            {
+                ExcludeComponentCorrelationHttpHeadersOnDomains =
+                {
+                    "core.windows.net",
+                    "core.chinacloudapi.cn",
+                    "core.cloudapi.de",
+                    "core.usgovcloudapi.net",
+                    "localhost",
+                    "127.0.0.1"
+                }
+            });
+
+            // for performance counter collection see: http://apmtips.com/blog/2015/10/07/performance-counters-in-non-web-applications/
+            AddAndInitializeModule<PerformanceCollectorModule>();
+
+            // for more information on QuickPulse see: http://apmtips.com/blog/2017/02/13/enable-application-insights-live-metrics-from-code/ 
+            var quickPulseModule = AddAndInitializeModule<QuickPulseTelemetryModule>();
+
+            TelemetryConfiguration.Active.TelemetryProcessorChainBuilder
+                .Use(next => new AutocollectedMetricsExtractor(next))
+                .Use(next =>
+                {
+                    var processor = new QuickPulseTelemetryProcessor(next);
+                    quickPulseModule.RegisterTelemetryProcessor(processor);
+                    return processor;
+                })
+                .UseAdaptiveSampling(AdaptiveSamplingMaxItemsPerSecond, excludedTypes: AdaptiveSamplingType)
+                .UseAdaptiveSampling(AdaptiveSamplingMaxItemsPerSecond, excludedTypes: null, includedTypes: AdaptiveSamplingType)
+                .Build();
+
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new BuildInfoConfigComponentVersionTelemetryInitializer());
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new DeviceTelemetryInitializer());
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new HttpDependenciesParsingTelemetryInitializer());
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new AzureRoleEnvironmentTelemetryInitializer());
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new AzureWebAppRoleEnvironmentTelemetryInitializer());
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new WebTestTelemetryInitializer());
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new SyntheticUserAgentTelemetryInitializer {Filters = "search|spider|crawl|Bot|Monitor|AlwaysOn" });
+
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new ClientIpHeaderTelemetryInitializer());
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new OperationNameTelemetryInitializer());
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new Microsoft.ApplicationInsights.Web.OperationCorrelationTelemetryInitializer());
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new UserTelemetryInitializer());
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new AuthenticatedUserIdTelemetryInitializer());
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new AccountIdTelemetryInitializer());
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new SessionTelemetryInitializer());
+
+            // ReSharper disable once UseObjectOrCollectionInitializer
+            TelemetryConfiguration.Active.TelemetryChannel = new ServerTelemetryChannel();
+#if DEBUG
+            SetDeveloperMode();
+#endif
+        }
+
+        public static void SetDeveloperMode()
+        {
+            TelemetryConfiguration.Active.TelemetryChannel.DeveloperMode = true;
+        }
+
+        public static void SetInstrumentationKey(string instrumentationKey)
+        {
+            if (string.IsNullOrWhiteSpace(instrumentationKey)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(instrumentationKey));
+
+            TelemetryConfiguration.Active.InstrumentationKey = instrumentationKey;
+        }
+
+        private static T AddAndInitializeModule<T>(Func<T> factory = null)
+            where T : ITelemetryModule, new()
+        {
+            var module = (factory != null) ? factory() : new T();
+            module.Initialize(TelemetryConfiguration.Active);
+            TelemetryModules.Instance.Modules.Add(module);
+            return module;
+        }
+    }
+}
