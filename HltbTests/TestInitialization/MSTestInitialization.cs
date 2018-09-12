c42ca5f5535d0335a08e61673427c1116ab96336
ohadschn <ohad188@gmail.com>
2018-09-12 14:04:13 +0300

    HltbsTests - fixed SSL secure channel issue

---
+++ HltbTests/TestInitialization/MSTestInitialization.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HltbTests.TestInitialization
+{
+    [TestClass]
+    public class MSTestInitialization
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        }
+        
+        [TestMethod]
+        public void TestSecurityProtocolAssignment()
+        {
+            // Dummy test required for AssemblyInit to be executed
+        }
+    }
+}
